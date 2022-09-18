70f482d45964958e83c5f9a3b5e5f35b3b067268
kjes9307 <kjes9307@gmail.com>
2022-09-18 18:12:50 +0800

    chore: fix mode

--- src/unauth/index.tsx
+++ src/unauth/index.tsx
@@ -18,7 +18,7 @@ export const UnAuthScreen = () => {
           <img src='/images/joseph-gonzalez-zcUgjyqEwe8-unsplash.jpg' alt="images" className="d-none d-lg-inline login-img" />
           </Col>
           <Col lg={7} className="d-lg-flex justify-content-center flex-column">
-            {mode?<LoginForm setMode={setModeChange} /> : <RegisterForm setMode={setModeChange} />}
+            {!mode?<LoginForm setMode={setModeChange} /> : <RegisterForm setMode={setModeChange} />}
           </Col>
         </Row>
         <UnAuthFooter />
