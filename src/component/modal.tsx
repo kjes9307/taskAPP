c865fc29dad0e45f38428c320e478e47bc8ddedd
kjes9307 <kjes9307@gmail.com>
2022-10-07 15:26:31 +0800

    fix primary color & modal

--- src/component/modal.tsx
+++ src/component/modal.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button,Container,Row,Col } from "react-bootstrap";
 import {useProjectModal,useProjectsQueryKey} from 'component/useModal'
 import { useAddName,useEditName } from "utils/project";
 import { useForm, SubmitHandler  } from 'react-hook-form';
+import "./modal.scss"
+import Icon from 'component/Icon'
 type Inputs = {
   name: string,
 };
@@ -16,7 +17,7 @@ export const ProjectModal = (props: {
       setValue ,
     } = useForm<Inputs>();
     const {projectModalOpen,close,detailData,isLoading}=useProjectModal()
-    const title = detailData? "Edit Item" : "Create Item";
+    const title = detailData? "Edit" : "Create";
     const useMutateProj = detailData ? useEditName : useAddName
     if(detailData){
       setValue("name",detailData?.[0].name || 'nodata')
@@ -33,32 +34,36 @@ export const ProjectModal = (props: {
         // close();
       })
     }
-
-    
     
     return (
     <>
-     
-      <Modal show={projectModalOpen} onHide={close}>
-      {isLoading?
-        <div className="spinner-border" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-        :
-        <Modal.Body>
-          <h1>{title}</h1>
-          <form onSubmit={handleSubmit(onSubmit)} className="d-flex align-items-center">
-          <input  {...register("name")}/>  
-          <Button type="submit" size="sm" className="ms-3">Edit</Button>
-          </form>
-        </Modal.Body>
-      }
-
-        <Modal.Footer>
-          <Button variant="secondary" onClick={close}>
-            Close
-          </Button>
-        </Modal.Footer>
+      <Modal show={projectModalOpen} onHide={close} contentClassName='modal-layout'>
+        <Container>
+        <Row>
+          <Col sm='12'>
+        <Modal.Title className="d-flex justify-content-between flex-row-reverse m-2">
+          <Icon icon='x' className="font-color" onClick={close} style={{cursor:"pointer"}} />
+          <h1 className="font-color">{title}</h1>
+        </Modal.Title>
+        {isLoading?
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+          :
+          <Modal.Body className="pt-0 pb-0">
+            <form onSubmit={handleSubmit(onSubmit)}>
+            <label className="font-color" htmlFor="taskName">修改名稱</label>
+              <div className="d-flex justify-content-between">
+                <input id='taskName'  {...register("name")} className='input-outline input-layout border-0 border-bottom' />  
+                <Button type="submit" size="sm" className="ms-3 text-white">Edit</Button>
+              </div>
+            </form>
+            <h6 className="font-color mt-2">卡片數量</h6>
+          </Modal.Body>
+        }
+          </Col>
+        </Row>
+        </Container>
       </Modal>
     </>
   );
