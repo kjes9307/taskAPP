fa20d6ee57425619697ad31830c1ecf7dcbd54ce
kjes9307 <kjes9307@gmail.com>
2022-10-12 19:14:50 +0800

    add group page

--- src/auth/Team/group.tsx
+++ src/auth/Team/group.tsx
@@ -1,25 +1,30 @@
 import {Col,Card,Button} from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 import moment from 'moment'
+import {useUserProjectList} from './util' 
 export const Group = () =>{
+    const {data} = useUserProjectList()
     return (
-        <Col sm='12' md='5'>
-            <Card>
-            <Card.Body>
-                <div className='d-flex align-items-center mb-2'>
-                    <Card.Title className='mb-0'>
-                        <img src="/images/andychen.jpeg" alt="test" className='avatar-img' />
-                    </Card.Title>
-                    <div className='ms-2'>
-                        <span className='text-dark fs-6'>Stickers : 活動</span>
-                        <Card.Subtitle className="text-muted fs-6">{moment(new Date()).format("YYYY.MM.DD HH:mm")}</Card.Subtitle>
-                    </div>
-                </div>
-                <Card.Text>
-                    Member of this stickers
-                </Card.Text>
-                <Button className='text-white ms-2'>Leave</Button>
-            </Card.Body>
-            </Card>
+        <Col sm='12' md='10'>
+            <h2>建立的項目</h2>
+            <div className='d-flex flex-wrap'> 
+            {data?.map(x=>(
+                <Card key={x._id} className='w-45 ms-2 mt-1'>
+                <Card.Body>
+                    <span className='text-dark fs-5'>Notes : {x.name}</span>
+                    <Card.Subtitle className="text-muted fs-6">{moment(new Date()).format("YYYY.MM.DD HH:mm")}</Card.Subtitle>
+                    <Card.Text className='mt-2'>
+                        Member of this notes
+                    </Card.Text>
+                    <Button size='sm' className='p-0'>
+                        <Link to={`/task/${x._id}/Event`} className='text-white text-decoration-none px-3'>前往</Link>
+                    </Button>
+                </Card.Body>
+                </Card>
+            ))}
+            </div>
+            <h2>被邀請的項目</h2>
+            
         </Col>
     )
 }
